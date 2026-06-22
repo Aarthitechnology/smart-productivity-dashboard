4c80b1b559a8c741aed78105195b43b82992696a
Aarthitechnology <aarthi908785@gmail.com>
2026-06-22 13:45:28 +0530

    Corrected Logout error

--- JS/Signup.js
+++ JS/Signup.js
@@ -1,25 +1,47 @@
 console.log("Signup.js loaded");
+
 import { auth } from "./Firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-const toast = document.getElementById("toast");
+
+import {
+    createUserWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 const signupBtn = document.getElementById("signupBtn");
-signupBtn.addEventListener("click",async () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+
+signupBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
     try {
-        await createUserWithEmailAndPassword(auth,email,password);
+
+        // Create Account
+        await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
         showToast("Account Created Successfully");
+
+        // Firebase automatically logs in the user after signup
+        // Sign out so that Login page appears
+        await signOut(auth);
 
         setTimeout(() => {
             window.location.href = "Login.html";
         }, 2000);
+
     }
-    catch(error)
-    {
+    catch (error) {
         showToast(error.message);
     }
+
 });
+
 function showToast(message) {
+
     const toast = document.getElementById("toast");
 
     toast.textContent = message;
@@ -29,4 +51,5 @@ function showToast(message) {
     setTimeout(() => {
         toast.classList.remove("show");
     }, 2000);
+
 }
